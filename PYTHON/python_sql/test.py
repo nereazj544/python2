bd7a8d2c0c1019172ea68d3927713d3a9e38d742
nereazj544 <nzjvirtual@gmail.com>
2025-06-02 14:01:46 +0200

    Refactorizar test.py para mejorar la búsqueda de lenguajes y su presentación; agregar manejo de errores al buscar lenguajes por nombre.

--- PYTHON/python_sql/test.py
+++ PYTHON/python_sql/test.py
@@ -9,5 +9,20 @@ collection = database_name['Lenguajes'] # Nombre de la colección
 doc = collection.find()
 
 for docmnt in doc:
-    print(docmnt)
+    # print(docmnt)
+    print (f"Lenguaje: {docmnt['nombre']}")
+print("Nombre del lenguaje a buscar: ")
+nombre = input().capitalize() # Solicitar al usuario el nombre del lenguaje a buscar
 
+# Buscar un documento específico por nombre y despues filtrar los paises donde se habla
+r = collection.find_one({'nombre': nombre}) # Buscar un documento específico
+if r:
+    print(f"Lenguaje encontrado: {r['nombre']}")
+    if 'paises' in r:
+        print("Pais(es) donde se habla:")
+        print(", ".join(r['paises']))
+else:
+    print("Lenguaje no encontrado.")
+
+
+rs = collection.find({'paises': 'España'}) # Buscar documentos donde se habla en España
