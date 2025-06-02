464e22c08f28a8f47836c6a641e542d45e97678d
nereazj544 <nzjvirtual@gmail.com>
2025-06-02 14:56:46 +0200

    Agregar implementación del servidor con manejo de conexiones y logging

---
+++ PYTHON/socket/server.py
@@ -0,0 +1,28 @@
+import socket
+from Logs import server_log, server_error, server_warning, server_debug, server_critical
+
+# CONFIGURACIÓN DEL SERVIDOR
+HOST = '127.0.0.1'
+PORT = 6000
+
+def start():
+    try:
+        print("Iniciando el servidor...")
+        server_log("Iniciando el servidor...")
+        # Código para iniciar el servidor
+        server_log("Iniciando el servidor en {}:{}".format(HOST, PORT))
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((HOST, PORT))
+        server.listen(5)  # Escuchar hasta 5 conexiones entrantes
+        server_log("Servidor iniciado y escuchando en {}:{}".format(HOST, PORT))
+        while True:
+            add, conn = server.accept()  # Aceptar una conexión entrante
+            print("Conexión aceptada desde:", add)
+            server_log("Conexión aceptada desde: {}".format(add))
+            
+
+
+    except Exception as e:
+        server_error(f"Error al iniciar el servidor: {e}")
+
+start()
